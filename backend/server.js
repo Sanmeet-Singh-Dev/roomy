dce9e510ff5cb6cabfd8ac10724df1829999a426
Raghav Khanna <raghavkhanna150@gmail.com>
2023-10-08 00:18:54 -0700

    chore: Initial backend setup and folder structure

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+const port = process.env.PORT || 6000;
+import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
+
+connectDB();
+
+const app = express();
+
+const allowedOrigins = ['http://192.168.x.x:3000']; // Replace with your iPhone's IP
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes)
+
+app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
