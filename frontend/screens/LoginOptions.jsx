1d41ee0648f02e01b14ef0672d0d72e10ed444b0
Anshul Gupta <anshulgupta@Anshuls-MacBook-Air.local>
2023-10-11 18:20:41 -0700

    chore: changhes to text

--- frontend/screens/LoginOptions.jsx
+++ frontend/screens/LoginOptions.jsx
@@ -16,7 +16,7 @@ const LoginOptions = () => {
           title='Register'
           onPress={() => navigation.navigate('register')}
         />
-        <Text>Hello</Text>
+        <Text>Hello2 test</Text>
       </SafeAreaView>
     </View>
   )
